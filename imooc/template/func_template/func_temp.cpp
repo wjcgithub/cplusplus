e7c8bba73e99696ba4a97a22ff0dfe0087de159a
wangjichao <wjc163@sina.cn>
2020-09-20 12:16:24 +0800

    func temp

--- imooc/template/func_template/func_temp.cpp
+++ imooc/template/func_template/func_temp.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 #include <stdlib.h>
 
+using namespace std;
+
+template <typename T>
+void display(T a)
+{
+    cout << a << endl;
+}
+
+template <typename T, class S>
+void display(T t, S s)
+{
+    cout << t << endl;
+    cout << s << endl;
+}
+
+template <typename T, int KSize>
+void display(T a)
+{
+    for (size_t i = 0; i < KSize; i++)
+    {
+        cout << a << endl;
+    }
+    
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    
+    //第一个模版
+    display<int>(10);
+    display<double>(10.89);
+    
+    display<int, double>(5, 8.3);
+
+    display<int, 5>(6);
+
     return 0;
 }
